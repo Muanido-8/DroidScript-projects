e066d8b79219c982054eea66f5e0d0df68997a20
Muanido-8 <66327276+Muanido-8@users.noreply.github.com>
2022-02-24 10:12:42 +0200

    Update Muanidos Caller.js

--- Simple-DroidScript-Caller/Muanidos Caller.js
+++ Simple-DroidScript-Caller/Muanidos Caller.js
@@ -5,7 +5,7 @@ lay = app.CreateLayout( "Linear", "Vertical,Center,VCenter" )
 app.Script("functions.js")
 app.Script("splash.js")
 app.Script("index.js")
-app.ChooseContact( "", function(a) { alert(a) } )
+
 
 app.SetOrientation( "portrait" )
 app.EnableBackKey( false )
